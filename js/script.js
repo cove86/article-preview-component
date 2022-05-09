d287e1f44e003c895ab8328300b19f19b1c963cf
Ashley Cove <ash.cove99@gmail.com>
2022-05-09 20:02:44 +0100

    Added new selector variable to script.js and various styling updates to styles.css

--- js/script.js
+++ js/script.js
@@ -2,24 +2,29 @@
 
 const shareBtn = document.querySelector('.share-btn');
 const shareDiv = document.querySelector('.share');
-const author = document.querySelector('.author');
+const author = document.querySelector('.author-div');
+const card = document.querySelector('.card');
+const text = document.querySelector('.card-text');
 
 let width = document.documentElement.clientWidth || window.innerWidth;
 
 shareBtn.addEventListener('click', () => {
   if (width > 780) {
-    shareDiv.style.display == 'none'
-      ? (shareDiv.style.display = 'flex')
-      : (shareDiv.style.display = 'none');
   } else {
-    // author.style.display = 'none';
-    author.innerHTML = `
+    author.style.display = 'none';
+    shareBtn.style.display = 'none';
+    text.style.paddingBottom = '0rem';
+    card.insertAdjacentHTML(
+      'beforeend',
+      `
     <div class="share-mobile">
     <p class="share-text">SHARE</p>
     <a class="fb-link" href="#"><img class="share-fb" src="images/icon-facebook.svg" alt="Facebook Logo"></a>
     <a class="tw-link" href="#"><img class="share-tw" src="images/icon-twitter.svg" alt="Twitter Logo"></a>
     <a class="pi-link" href="#"><img class="share-pi" src="images/icon-pinterest.svg" alt="Pinterest Logo"></a>
+    <a class="share-btn"><img class="share-icon" src="images/icon-share.svg" /></a>
   </div>
-    `;
+    `
+    );
   }
 });
