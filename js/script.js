3f0c698da4fb320f1a2990abbfdf0e9a42377073
Ashley Cove <ash.cove99@gmail.com>
2022-05-09 16:38:12 +0100

    Added positioning for share div and a box-shadow, added function to script.js to open and close the share pop up

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'Use Strict';
+
+const shareBtn = document.querySelector('.share-btn');
+const shareDiv = document.querySelector('.share');
+
+shareBtn.addEventListener('click', () =>
+  shareDiv.style.display == 'none'
+    ? (shareDiv.style.display = 'flex')
+    : (shareDiv.style.display = 'none')
+);
